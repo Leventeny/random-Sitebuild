faf42b650dc58b4393af81f041c97915b0ae9800
Levente Nyikus <levente.nyikus@gmail.com>
2021-09-28 11:47:35 +0200

    Init css

--- randomBuild_with_Vue_petite/main.js
+++ randomBuild_with_Vue_petite/main.js
@@ -1,6 +1,6 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+import { createApp } from "https://unpkg.com/petite-vue?module";
+createApp().mount('#validationForm');
+
+
