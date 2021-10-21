2a4d5e0fb031277c97a5ddac8dd1daca47489667
Levente Nyikus <levente.nyikus@gmail.com>
2021-10-21 23:42:45 +0200

    Headline added

--- randomBuild_with_Vue_petite/main.js
+++ randomBuild_with_Vue_petite/main.js
@@ -3,4 +3,12 @@ import './style.css'
 import { createApp } from "https://unpkg.com/petite-vue?module";
 createApp().mount('#validationForm');
 
+const registrationButton = document.querySelector('.registrationButton');
+
+registrationButton.addEventListener('click', () => {
+    console.log('Hmm strange');
+})
+
+
+
 
